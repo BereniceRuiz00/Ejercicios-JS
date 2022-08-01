e173c66f612562cfb3a3bdea224083f86738138d
BereniceRuiz00 <ruizsilviabere@gmail.com>
2022-08-01 17:03:16 -0500

    Se crea nuevo archivo js de nombre variables, se describe los diferentes variables, number, bolean, undefined, null. En el index se cambia src de script.js a variable.js

---
+++ assets/js/variables.js
@@ -0,0 +1,67 @@
+// Mejores prácticas
+
+/* camelCase
+Utilizar mayúsculas en la mitad de nuestras variabeles
+Ejemplos: variableTriangulo, miNombre, miEdad
+*/
+
+/* Palabras reservadas
+No usar palabras reservadas para declarar una variable
+Ejemplos: if, return, true, false, for, while, let, var, etc.
+var while;
+var null ;
+var if;
+var true;
+var false;
+var var;
+var const;
+var let;
+*/
+
+/* Seguir standar personal
+Pueden contener números, letras, guion bajo, simbolo de dolar
+*/
+
+var a;
+var a1;
+var a_Nombre;
+var a$Direccion;
+
+
+// Declaración de variables
+
+// Type number
+var edad = 22;
+var peso = 86;
+var miopia = 1.5;
+
+// Type Boolean
+var ciego = true;
+var hambre = true;
+var bateria = false;
+
+// Type String
+var nombre = "Berenice Ruiz";
+var dir = "Guadalupe, Nuevo León";
+var nacionalidad = "Mexicana";
+
+// Type Null
+var ganasDeVivir = null;
+var nineros = null;
+
+// Variables declaradas con un mismo valor
+var x, y, z = 4;
+var uno = dos = tres = 4;
+
+// Variables undefined
+var xx, yy, zz;
+
+// Escribir en una sola linea variables undefined de nuestro carrito de compras
+
+var producto;
+var cantProducto;
+var precioProducto;
+var precioSubtotal;
+var precioTotal;
+var metodoDePAgo;
+
