da46fa04d99130138bf6459d719cbde8376ecb78
BereniceRuiz00 <ruizsilviabere@gmail.com>
2022-08-01 15:53:22 -0500

    Ejercicio javascript alert, console, link antes de la etiqueta final de body, caso upper and low

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+// Primer JS
+// JS es case sensitive
+
+// alerta mesaje flotante
+// alert ("Hello\nHow are you?");
+
+// promt - mensaje flotante con ingreso de texto
+
+// guardamos la variable como nombre y que se muestre en la consola
+// nombre = prompt("Ingresa un valor");
+
+// consola - inpeccion
+// console.log(nombre);
+
+// CamelCase
+// Buena practica empezar con LOWER a variables con mas de una palabra y las que siguen con UPPER 
+// UpperCamelCase - ComidaFavoritaDeFelipe -lowerCamelCase  - comidaFavoritaDeFelipe
+
+// Las variables deben de ser cortas pero especificas
+// nombreDeVariable = 52;
+
+// Ejemplo variable triangulo
+
+altura = prompt("Area de un triangulo \n Ingresa altrura");
+base= prompt("Ingresa la base del triangulo");
+
+areaTriangulo = (base*altura)/2;
+
+alert ("Area del Triangulo = "+areaTriangulo);
+console.log("Base:"+base+"\nAltura:"+altura+"\nArea del triangulo: "+areaTriangulo);
+
+// Funciones
+/* Variables globales: se pueden utilizar ara todo el doc o en varias funciones
+Variables locales: variables propias de una funcion 
+*/
